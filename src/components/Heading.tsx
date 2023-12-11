23be6598841f93b7f5836cc86958230b31097df8
Ashwin <ashwinkv.akv@gmail.com>
2023-12-11 20:29:58 +0530

    chalv tracker

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -4,7 +4,7 @@ interface Props{
 function Heading({children}:Props){
     return(
         <div className="text-center w-screen m-5">
-        <h1 className="text-3xl mt-10 text-bold font-[poppins]">{children}</h1>
+            <h1 className="text-3xl mt-10 text-bold font-[Montserrat]">{children}</h1>
         </div>
     )
 }
